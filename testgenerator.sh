30386da40c685a8d43d70e5e45acacd23e81ce70
Tomáš Čejka <tomas@kassoft.eu>
2010-10-06 10:05:14 +0200

    New test files

---
+++ testgenerator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Enter filename eg. test5: " FILENAME
+read -p "Enter number of points: " AMOUNT
+
+if [ -e "testdata/$FILENAME" ]; then
+  read -p "File $FILENAME will be overwritten. For cancel press CTRL+C"
+fi
+
+echo "$AMOUNT" > "testdata/$FILENAME"
+for ((i=0; i<$AMOUNT; i++)); do
+  echo "$((RANDOM/100)) $((RANDOM/100))" >> "testdata/$FILENAME"
+done
+
